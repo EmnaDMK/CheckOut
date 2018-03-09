1ccda2516565d68182df0b88cffd2e82a8dcf763
Emna Dammak <e.dammak@root.sa>
2018-03-09 15:07:48 +0100

    Add project files.

---
+++ CheckOut/CheckOut/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CheckOut
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            MainPage = new CheckOut.MainPage();
+            //MainPage = new NavigationPage(new CheckOut.PayementAndShipping());
+
+
+           
+        }
+	
+
+        protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ CheckOut/CheckOut/MainPage.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CheckOut
+{
+    public partial class MainPage : ContentPage
+    {
+
+        // public List<Product> ProdList;
+
+        public ObservableCollection<Product> ProdList { get; set; }
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            GetProduct();
+            ListProducts.ItemsSource = ProdList;
+            //BindingContext = this;
+
+
+            //if (BindingContext.Equals(Button_OnBackStep1Clicked()))
+            //{
+            //    if (stepBar.StepSelected.Equals(1))
+            //    {
+            //        BindingContext = Step1Layout;
+            //    }
+            //}
+        }
+
+
+        void GetProduct()
+        {
+            ProdList = new ObservableCollection<Product>
+            {
+
+                new Product
+                {
+                    Name = "Gucci er",
+                    ShortDescription = "Almost New",
+                    Price = "3000.00 SAR",
+                    ImageURL = "Pic1.png"
+                },
+                new Product
+                {
+                    Name = "Gucci Hand Bag Light Brown",
+                    ShortDescription = "Almost New",
+                    Price = "3000.00 SAR",
+                    ImageURL = "Pic1.png"
+                },
+
+                new Product
+                {
+                    Name = "Gucci er",
+                    ShortDescription = "Almost New",
+                    Price = "3000.00 SAR",
+                    ImageURL = "Pic1.png"
+                }
+            };
+        }
+
+
+
+        private void Button_OnBackStep3Clicked(object sender, EventArgs e)
+
+        {
+
+            Step2Layout.IsVisible = true;
+
+            Step3Layout.IsVisible = false;
+
+            Step1Layout.IsVisible = false;
+
+        }
+
+        private void Button_OnNextStep3Clicked(object sender, EventArgs e)
+
+        {
+
+
+
+        }
+
+        private void Button_OnBackStep2Clicked(object sender, EventArgs e)
+
+        {
+
+            Step2Layout.IsVisible = false;
+
+            Step3Layout.IsVisible = false;
+
+            Step1Layout.IsVisible = true;
+
+        }
+
+        private void Button_OnNextStep2Clicked(object sender, EventArgs e)
+
+        {
+
+            Step2Layout.IsVisible = false;
+
+            Step3Layout.IsVisible = true;
+
+            Step1Layout.IsVisible = false;
+
+        }
+
+        private void Button_OnBackStep1Clicked(object sender, EventArgs e)
+
+        {
+
+        }
+
+        private void Button_OnNextStep1Clicked(object sender, EventArgs e)
+
+        {
+
+            Step2Layout.IsVisible = true;
+
+            Step3Layout.IsVisible = false;
+
+            Step1Layout.IsVisible = false;
+
+          
+        }
+
+
+
+        private void TapGestureRecognizer_OnCreditCardTapped(object sender, EventArgs e)
+
+        {
+
+            CreditCardFrame.BackgroundColor = Color.Black;
+
+            BankTransferFrame.BackgroundColor = Color.Gainsboro;
+
+        }
+
+        private void TapGestureRecognizer_OnBankTransferTapped(object sender, EventArgs e)
+
+        {
+
+            CreditCardFrame.BackgroundColor = Color.Gainsboro;
+
+            BankTransferFrame.BackgroundColor = Color.Black;
+
+        }
+
+
+
+        private void TapGestureRecognizer_OnExpressShippingTapped(object sender, EventArgs e)
+
+        {
+
+            ExpressShippingFrame.BackgroundColor = Color.Black;
+
+            NormalShippingFrame.BackgroundColor = Color.Gainsboro;
+
+        }
+
+        private void TapGestureRecognizer_OnNormalShippingTapped(object sender, EventArgs e)
+
+        {
+
+            ExpressShippingFrame.BackgroundColor = Color.Gainsboro;
+
+            NormalShippingFrame.BackgroundColor = Color.Black;
+
+        }
+
+        private void TapGestureRecognizerDel_OnTapped(object sender, EventArgs e)
+
+        {
+
+
+            // var button = sender as Button;
+            var remove = sender as Image;
+            var product = remove.BindingContext as Product;
+            ProdList.Remove(product);
+
+
+
+
+
+
+
+            // public static BindableProperty ItemClickCommandProperty = BindableProperty.Create(nameof(ItemClickCommand), typeof(ICommand), typeof(Product), null);
+
+
+
+            // public ICommand ItemClickCommand
+
+            // {
+
+            //     get { return (ICommand) this.GetValue(ItemClickCommandProperty); }
+
+            //     set { this.SetValue(ItemClickCommandProperty, value); }
+            // }
+
+
+
+        }
+    }
+}
+
